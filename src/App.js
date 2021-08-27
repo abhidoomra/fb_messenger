7dea42d1e663e302bcb03b0cb35ace74b2c149cb
abhidoomra <abhinandan0060.cse19@chitkara.edu.in>
2021-08-27 21:34:43 +0530

    adding username to message(object)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { Button, FormControl, FormHelperText, InputLabel, Input } from '@material-ui/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Message from './Message';
 
 function App() {
-  const [message, setMessage] = useState([]);
+  //storing username and set username
+  const [username, setUsername] = useState('');
+  //calling on loading page and ask for user
+  useEffect(() => {
+    setUsername(prompt("Enter username: "))
+  }, [])
+
+  //store message as a array of object having username and text
+  const [messages, setMessages] = useState([]);
+  //take input that is text of message
   const [input, setInput] = useState('');
+
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessage([...message, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput('');
   }
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Facebook Messenger</h1>
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>Enter Message</InputLabel>
@@ -22,7 +33,7 @@ function App() {
         </FormControl>
       </form>
       {
-        message.map(message => (<Message text={message} />))
+        messages.map(message => (<Message username={message.username} text={message.text} />))
       }
     </div>
   );
