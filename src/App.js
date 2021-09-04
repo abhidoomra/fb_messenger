b22191a9ac746aeeb59d54ceec2c8492dd7986f9
abhidoomra <abhinandan0060.cse19@chitkara.edu.in>
2021-09-05 01:37:23 +0530

    style to messenger

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SendIcon from '@material-ui/icons/Send';
 import db from './firebase';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import { green } from '@material-ui/core/colors';
+import { Colorize } from '@material-ui/icons';
 
 
 function App() {
@@ -46,27 +48,35 @@ function App() {
     })
     // setMessages([...messages, { username: username, text: input }]);
     setInput('');
+    document.documentElement.scrollTop = 0;
   }
   return (
     <div className="App">
-      <h1>Facebook Messenger</h1>
-      <h2>Welcome {username}</h2>
-      <form className="app__form">
-        <FormControl className="app__formcontrol">
-          <Input className="app__input" placeholder="Enter message" value={input} onChange={event => setInput(event.target.value)} />
-          <IconButton className="app__iconButton" type="submit" disabled={!input} variant="contained" color="primary" onClick={sendMessage}>
-            <SendIcon />
-          </IconButton>
-          {/* icon button wrap sendicon and make it as button */}
-        </FormControl>
-      </form>
-      <FlipMove>
-        {
-          // prompt username is passed and whole object message is passed.
-          messages.map(({ id, message }) => (<Message key={id} username={username} text={message} />))
-          // key is to render only new value not old
-        }
-      </FlipMove>
+      <div className="app__header">
+        <h1 style={{ marginTop: 0 }}>Messenger
+        </h1>
+        <h2>Welcome {username}</h2>
+      </div>
+      <div className="app__messageContainer">
+        <FlipMove>
+          {
+            // prompt username is passed and whole object message is passed.
+            messages.map(({ id, message }) => (<Message key={id} username={username} text={message} />))
+            // key is to render only new value not old
+          }
+        </FlipMove>
+      </div>
+      <div className="app__formContainer">
+        <form className="app__form">
+          <FormControl className="app__formcontrol">
+            <input className="app__input" placeholder="Enter message" value={input} onChange={event => setInput(event.target.value)} />
+            <IconButton className="app__iconButton" type="submit" disabled={!input} variant="contained" color="" onClick={sendMessage}>
+              <SendIcon />
+            </IconButton>
+            {/* icon button wrap sendicon and make it as button */}
+          </FormControl>
+        </form>
+      </div>
     </div>
   );
 }
